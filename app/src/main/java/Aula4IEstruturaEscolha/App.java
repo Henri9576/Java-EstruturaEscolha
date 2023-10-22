e5492a0ffae61ad24802fcec5855113f9100e9a1
Henrique9576 <henrique_costa02@outlook.com>
2023-10-22 17:31:16 -0300

    Initial commit

---
+++ app/src/main/java/Aula4IEstruturaEscolha/App.java
@@ -0,0 +1,22 @@
+package Aula4IEstruturaEscolha;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        int codigoProduto = 2;
+        
+        switch(codigoProduto){
+        
+            case 1:
+                //comandos caso seja 1
+                break;
+            case 2:
+                //comandos caso seja 2
+                break;
+            default:
+                //comandos caso seja outro
+        }
+        
+    }
+}
